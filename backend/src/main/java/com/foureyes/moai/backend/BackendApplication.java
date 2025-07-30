23a796c2d922b00c2e46a922ba0f16f5dccb7c83
JeongHyunHeo <jeonghyunheo619@gmail.com>
2025-07-30 10:03:59 +0900

    chore : fixed issue of unable to run Spring Boot Application on docker container, by adding missing annotations and dependency

--- backend/src/main/java/com/foureyes/moai/backend/BackendApplication.java
+++ backend/src/main/java/com/foureyes/moai/backend/BackendApplication.java
@@ -1,7 +1,9 @@
 package com.foureyes.moai.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
--- backend/src/main/java/com/foureyes/moai/backend/domain/user/mapper/UserMapper.java
+++ backend/src/main/java/com/foureyes/moai/backend/domain/user/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.foureyes.moai.backend.domain.user.mapper;
 
 import com.foureyes.moai.backend.domain.user.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper {
     void insertUser(User user);
     User findByEmail(String email);
