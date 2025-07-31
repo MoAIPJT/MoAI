e3cec9900b068a4fbe946724802e1382296e4c87
JeongHyunHeo <jeonghyunheo619@gmail.com>
2025-07-31 09:31:14 +0900

    S13P11B201-162 ADD: Testing ID/PW login API

--- backend/src/main/java/com/foureyes/moai/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/foureyes/moai/backend/domain/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.foureyes.moai.backend.domain.user.controller;
 
+import com.foureyes.moai.backend.domain.user.dto.request.LoginRequest;
 import com.foureyes.moai.backend.domain.user.dto.request.UserSignupRequest;
+import com.foureyes.moai.backend.domain.user.dto.response.LoginResponse;
 import com.foureyes.moai.backend.domain.user.dto.response.UserSignupResponse;
 import com.foureyes.moai.backend.domain.user.service.UserService;
 import jakarta.validation.Valid;
@@ -16,9 +18,14 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/signup")
-    public ResponseEntity<UserSignupResponse> signup(@Valid @RequestBody UserSignupRequest request) {
-        return ResponseEntity.ok(userService.signup(request));
+//    @PostMapping("/signup")
+//    public ResponseEntity<UserSignupResponse> signup(@Valid @RequestBody UserSignupRequest request) {
+//        return ResponseEntity.ok(userService.signup(request));
+//    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(userService.login(request));
     }
 }
 
--- backend/src/main/java/com/foureyes/moai/backend/domain/user/exception/ErrorCode.java
+++ backend/src/main/java/com/foureyes/moai/backend/domain/user/exception/ErrorCode.java
@@ -2,6 +2,8 @@ package com.foureyes.moai.backend.domain.user.exception;
 
 public enum ErrorCode {
     EMAIL_ALREADY_EXISTS("이미 존재하는 이메일입니다."),
+    USER_NOT_FOUND("존재하지 않는 회원입니다."),
+    INVALID_PASSWORD("비밀번호가 일치하지 않습니다."),
     INVALID_REQUEST("잘못된 요청입니다.");
 
     private final String message;
