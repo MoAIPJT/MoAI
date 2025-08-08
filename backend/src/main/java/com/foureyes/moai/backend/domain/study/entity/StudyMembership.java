84eb7a33a5597103a196eed67ffeb73cbb1f7cce
오현석 <dhgustjr29@gmail.com>
2025-08-08 11:29:44 +0900

    docs: code commentation add

--- backend/src/main/java/com/foureyes/moai/backend/domain/study/entity/StudyMembership.java
+++ backend/src/main/java/com/foureyes/moai/backend/domain/study/entity/StudyMembership.java
@@ -18,20 +18,22 @@ public class StudyMembership {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "user_id")
+    @Column(name = "user_id", nullable = false)
     private int userId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "study_id")
+    @JoinColumn(name = "study_id", nullable = false)
     private StudyGroup studyGroup;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Status status;
 
-    @Column(name = "joined_at")
+    @Column(name = "joined_at", nullable = false)
     private LocalDateTime joinedAt;
 
     public enum Role {
