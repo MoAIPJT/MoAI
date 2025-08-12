3c6328db540884f09ec9c0c642a800d8818e6862
허정현 <jeonghyunheo619@gmail.com>
2025-08-12 13:08:54 +0900

    fix : add PATCH method to allowed method in spring security

--- backend/src/main/java/com/foureyes/moai/backend/commons/config/WebSecurityConfig.java
+++ backend/src/main/java/com/foureyes/moai/backend/commons/config/WebSecurityConfig.java
@@ -69,7 +69,7 @@ public class WebSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("https://localhost", "http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(
             Arrays.asList("Authorization", "Content-Type", "X-Requested-With")
         );
