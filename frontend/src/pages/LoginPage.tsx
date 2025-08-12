916c99089edc18e5e7f8b131b56ac534bd9821ae
허정현 <jeonghyunheo619@gmail.com>
2025-08-12 14:25:58 +0900

    fix: google login format like kakao login

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -11,16 +11,13 @@ const LoginPage: React.FC = () => {
   };
 
   const handleKakaoLogin = () => {
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
     window.location.href = `${backendUrl}/oauth2/authorization/kakao`
   }
 
   const handleGoogleLogin = () => {
-    const googleClientId = import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID;
-    const redirectUri = `http://localhost:5173/auth/google/callback`;
-    const scope = `https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid`;
-    const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleClientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
-    window.location.href = googleAuthUrl;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    window.location.href = `${backendUrl}/oauth2/authorization/google`;
   }
 
   return (
